97396dcbef267bfa0e9fcbb172842896703df486
gramozilho <miguelsemartins+1@gmail.com>
2020-03-28 17:55:48 +0000

    restarting and level progression fixed

--- Objects/MenuHandler.gd
+++ Objects/MenuHandler.gd
@@ -1,23 +1,28 @@
 extends CanvasLayer
 
 var menu_on := false setget menu_transition
-var current_scene := null
+onready var current_scene
 var can_open_menu := true
 var current_level := 0
 
 var path_titlescreen := "res://Scenes/TitleMenu.tscn"
 var path_lvl1 := "res://Scenes/Level 1.tscn"
 var path_lvl2 := "res://Scenes/Level 2.tscn"
-var path_levels := [path_lvl1, path_lvl2]
+var path_endscreen := "res://Scenes/EndScreen.tscn"
+var path_levels := [path_lvl1, path_lvl2, path_endscreen]
 
 const MSG_LEVEL_COMPLETED := "Level completed!"
 const MSG_LEVEL_FAILED := "Level failed :("
 
 func _ready() -> void:
 	reset_menus()
-	
+	update_current_scene()
+
+
+func update_current_scene():
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
+	print("current_scene: ", current_scene)
 
 func _process(delta : float) -> void:
 	# keeping the menu handling here for now to keep my work on separate scenes
@@ -47,13 +52,14 @@ func reset_menus() -> void:
 
 func _on_Restart_pressed() -> void:
 	reset_menus()
-	get_tree().reload_current_scene()
+	goto_scene(path_levels[current_level])
 	AntManager.amount_of_ants = 0
 	#goto_scene(path_levels[current_level])
 
 
 func _on_BackToMenu_pressed() -> void:
 	goto_scene(path_titlescreen)
+
 
 func goto_scene(path) -> void:
 	call_deferred("_deferred_goto_scene", path)
@@ -62,7 +68,7 @@ func goto_scene(path) -> void:
 		$GUI.visible = false
 
 func _deferred_goto_scene(path) -> void:
-	current_scene.call_deffered("free")
+	current_scene.call_deferred("free")
 	var s = ResourceLoader.load(path)
 	current_scene = s.instance()
 	get_tree().get_root().add_child(current_scene)
@@ -83,9 +89,17 @@ func lose_screen() -> void:
 	
 
 func _on_Next_pressed() -> void:
-	current_level = (current_level + 1) % len(path_levels)
+	#current_level = (current_level + 1) % len(path_levels)
+	current_level += 1
 	goto_scene(path_levels[current_level])
+	if current_level == len(path_levels):
+		can_open_menu = false
 
 func start_game() -> void:
 	current_level = 0
 	goto_scene(path_levels[current_level])
+
+func end_game() -> void:
+	current_level = 0
+	goto_scene(path_titlescreen)
+	
