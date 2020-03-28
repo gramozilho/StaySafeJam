00a8005a721e87f255df3b136b0a08351f65ec9d
Jamination <jheasman81@gmail.com>
2020-03-28 11:49:45 +0000

    Fixed stuff.

--- Objects/Ant.gd
+++ Objects/Ant.gd
@@ -11,8 +11,9 @@ var _speed := MOVE_SPEED
 onready var _floor_cast := $FloorCast
 onready var _jump_before_land_timer := $JumpBeforeLandTimer
 onready var _jump_after_fallen_timer := $JumpAfterFallenTimer
+onready var _wall_cast := $WallCast
 
-var _velocity : Vector2
+var _velocity := Vector2()
 
 var _is_floorcast_touching := false
 var _is_falling := false
@@ -23,12 +24,20 @@ var _has_jumped := false
 enum MOVEMENT_STATES {
 	NORMAL,
 	FROZEN,
-	DEAD,
 }
 
 var state = MOVEMENT_STATES.NORMAL
 
 func _ready() -> void:
+	pass
+
+func die() -> void:
+	var camera : Camera2D = get_node("Camera2D")
+	if (is_instance_valid(camera)):
+		remove_child(camera)
+	
+	AntManager.add_ant(self, camera, Vector2(320, 296))
+	call_deferred("free")
 	pass
 
 func _physics_process(delta : float) -> void:
@@ -81,6 +90,9 @@ func _movement(delta : float) -> void:
 	
 	if (is_on_ceiling()):
 		_velocity.y = GRAVITY
+	
+	if (global_position.y > OS.window_size.y):
+		die()
 	
 	if (Input.is_action_just_pressed("move_jump") && !_has_jumped && (_is_floorcast_touching || _jump_after_fallen_timer.time_left > 0.0)):
 		_velocity.y = JUMP_HEIGHT
