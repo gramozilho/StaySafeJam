b5911c9bcc19bf707b36c1fcbed76368ea875943
Jamination <jheasman81@gmail.com>
2020-03-28 10:36:25 +0000

    Added the ability to freeze the ants.

--- Objects/Ant.gd
+++ Objects/Ant.gd
@@ -2,32 +2,50 @@ extends KinematicBody2D
 class_name Ant
 
 const GRAVITY := 6000.0
-const MOVE_SPEED := 40000.07
+const MOVE_SPEED := 30000.0
 const JUMP_HEIGHT := -100000
 const ACCELERATION : = .25
 
 var _speed := MOVE_SPEED
 
 onready var _floor_cast := $FloorCast
-onready var _wall_cast := $WallCast
+onready var _jump_before_land_timer := $JumpBeforeLandTimer
+onready var _jump_after_fallen_timer := $JumpAfterFallenTimer
 
 var _velocity : Vector2
 
 var _is_floorcast_touching := false
 
+enum MOVEMENT_STATES {
+	NORMAL,
+	FROZEN,
+}
+
+var state = MOVEMENT_STATES.NORMAL
+
 func _ready() -> void:
 	pass
 
 func _physics_process(delta : float) -> void:
-	_movement(delta)
-	move_and_slide(_velocity * delta, _floor_cast.get_collision_normal())
+	match (state):
+		MOVEMENT_STATES.NORMAL:
+			if (Input.is_action_just_pressed("freeze_ant") && _is_floorcast_touching):
+				state = MOVEMENT_STATES.FROZEN
+				$Sprite.modulate = Color.red
+				
+				var camera : Camera2D = get_node("Camera2D")
+				if (is_instance_valid(camera)):
+					remove_child(camera)
+				
+				AntManager.add_ant(camera, Vector2(320, 296))
+			_movement(delta)
+			move_and_slide(_velocity * delta, Vector2.UP)
+		MOVEMENT_STATES.FROZEN:
+			pass
 	pass
 
 func _movement(delta : float) -> void:
 	var direction := int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
-	_wall_cast.position.x = direction * 32
-	_wall_cast.scale.x = direction
 	
 	_velocity.x = lerp(_velocity.x, direction * MOVE_SPEED, ACCELERATION)
 	
