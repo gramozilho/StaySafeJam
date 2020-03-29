5011e0f8c9466fc1e6d13d8d280e5b5c05ea79ae
gramozilho <miguelsemartins+1@gmail.com>
2020-03-29 19:15:54 +0100

    endscreen button fix

--- GUI/CutscenePlayer.gd
+++ GUI/CutscenePlayer.gd
@@ -2,6 +2,7 @@ extends Control
 
 var current_slide := 0
 onready var slide_list := []
+var click_once = true
 
 func _ready():
 	slide_list = $Screen.get_children()
@@ -17,5 +18,7 @@ func _on_Next_pressed():
 	if current_slide < (slide_list.size()-1):
 		current_slide += 1
 		slide_list[current_slide].visible = true
-	else:
+	elif click_once:
+		click_once = false
 		MenuHandler.out_of_cutscene()
+	pass
--- Scenes/EndScreen.gd
+++ Scenes/EndScreen.gd
@@ -1,6 +1,8 @@
 extends Control
 
 func _ready():
+	$VBoxContainer/Next.visible = true
+	$VBoxContainer/Back.visible = false
 	MenuHandler.hide_GUI()
 
 func _on_Back_pressed():
@@ -14,4 +16,9 @@ func _on_StartGame_pressed():
 func _on_Exit_pressed():
 	get_tree().quit()
 
+
+func _on_Next_pressed():
+	$Cutscene/AnimationPlayer.play('fall')
+	$VBoxContainer/Next.visible = false
+	$VBoxContainer/Back.visible = true
 	
