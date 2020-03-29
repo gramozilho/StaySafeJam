96469d67f94f14919e42def4dbc065171dee4251
gramozilho <miguelsemartins+1@gmail.com>
2020-03-29 20:08:38 +0100

    cutscene player done

--- Scenes/EndScreen.gd
+++ Scenes/EndScreen.gd
@@ -9,15 +9,8 @@ func _on_Back_pressed():
 	MenuHandler.end_game()
 
 
-func _on_StartGame_pressed():
-	MenuHandler.start_game()
-
-
-func _on_Exit_pressed():
-	get_tree().quit()
-
-
 func _on_Next_pressed():
+	$Button.play()
 	$Cutscene/AnimationPlayer.play('fall')
 	$VBoxContainer/Next.visible = false
 	$VBoxContainer/Back.visible = true
